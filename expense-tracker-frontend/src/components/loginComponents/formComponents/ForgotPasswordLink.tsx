9bc273dcd14f50361ca71b82224afa55d305232b
Andrew.I <andrewiheanacho811@gmail.com>
2025-08-07 08:57:33 -0500

    Reverted to MUI link

--- expense-tracker-frontend/src/components/loginComponents/formComponents/ForgotPasswordLink.tsx
+++ expense-tracker-frontend/src/components/loginComponents/formComponents/ForgotPasswordLink.tsx
@@ -1,9 +1,8 @@
 import { Box, Link } from '@mui/material';
-import NextLink from 'next/link';
 
 const ForgotPasswordLink = () => (
   <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-    <Link component={NextLink} href="/forgot-password" color="primary" variant="body2">
+    <Link href="/forgot-password" color="primary" variant="body2">
       Forgot password?
     </Link>
   </Box>
